9b918cf4e6bbbcc04cc1efc2e90c4ce8a592c345
谭嘉俊 <1120571286@qq.com>
2020-10-08 16:49:13 +0800

    添加注释。

--- app/src/main/java/com/tanjiajun/viewdemo/MainActivity.kt
+++ app/src/main/java/com/tanjiajun/viewdemo/MainActivity.kt
@@ -12,9 +12,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        // 创建子线程，并且启动它
         Thread {
             // 让线程睡眠一秒
 //            Thread.sleep(1000)
+            // 在子线程更新UI
             findViewById<TextView>(R.id.tv_content).text = "谭嘉俊"
         }.start()
     }
